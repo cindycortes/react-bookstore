5c12f824b3d01152d426b05a2fe06236b4ce6658
Cindy Cortes <cindy.indins@gmail.com>
2019-02-05 19:07:34 -0700

    implemented GET using axios

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import SearchBar from './components/SearchBar';
 
 
 class App extends React.Component {
-  
+
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', { // takes two arguments, first the Url, second is the object
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 56902a12749454684bb59e187ffbb5a308be21ea1d76a7efb0969890f93da9e4'
+
+      }
+    });
   }
-  
+
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
